f9410301f107205d0383d8254c1a11d4eb2b7711
bcalder <calderbg@gmail.com>
2017-02-23 22:08:43 -0800

    Initial Commit

---
+++ KeyEventsSwift/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  KeyEventsSwift
+//
+//  Created by Bruce Calder on 2/23/17.
+//  Copyright © 2017 Bruce Calder. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ KeyEventsSwift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  KeyEventsSwift
+//
+//  Created by Bruce Calder on 2/23/17.
+//  Copyright © 2017 Bruce Calder. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
